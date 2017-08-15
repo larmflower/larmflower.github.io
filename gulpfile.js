796765fa51803b801117cfc4160944141cd23b63
larmflower <lnarosetta@gmail.com>
2017-08-15 17:39:06 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -11,30 +11,30 @@ var browserSync = require('browser-sync');
 var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
 
 /*
- * Build the Jekyll Site
- * runs a child process in node that runs the jekyll commands
- */
+* Build the Jekyll Site
+* runs a child process in node that runs the jekyll commands
+*/
 gulp.task('jekyll-build', function (done) {
-	return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
-		.on('close', done);
+  return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
+  .on('close', done);
 });
 
 /*
- * Rebuild Jekyll & reload browserSync
- */
+* Rebuild Jekyll & reload browserSync
+*/
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-	browserSync.reload();
+  browserSync.reload();
 });
 
 /*
- * Build the jekyll site and launch browser-sync
- */
+* Build the jekyll site and launch browser-sync
+*/
 gulp.task('browser-sync', ['jekyll-build'], function() {
-	browserSync({
-		server: {
-			baseDir: '_site'
-		}
-	});
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
 });
 
 /*
@@ -42,37 +42,37 @@ gulp.task('browser-sync', ['jekyll-build'], function() {
 */
 gulp.task('sass', function() {
   gulp.src('src/styles/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(csso())
-    .pipe(gulp.dest('assets/css'));
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(csso())
+  .pipe(gulp.dest('assets/css'));
 });
 
 /*
- * Minify images
- */
+* Minify images
+*/
 gulp.task('imagemin', function() {
-	return gulp.src('src/img/**/*.{jpg,png,gif}')
-		.pipe(plumber())
-		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('assets/img/'));
+  return gulp.src('src/img/**/*.{jpg,png,gif}')
+  .pipe(plumber())
+  .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+  .pipe(gulp.dest('assets/img/'));
 });
 
 /**
- * Compile and minify js
- */
+* Compile and minify js
+*/
 gulp.task('js', function(){
-	return gulp.src('src/js/**/*.js')
-		.pipe(plumber())
-		.pipe(concat('main.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('assets/js/'))
+  return gulp.src('src/js/**/*.js')
+  .pipe(plumber())
+  .pipe(concat('main.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('assets/js/'))
 });
 
 gulp.task('watch', function() {
   gulp.watch('src/styles/**/*.scss', ['sass']);
   gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
+  gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
   gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
 });
 
